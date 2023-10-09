92a413ea0e7f065318ba18355dfbc031b0ff03a3
CharlyWells <wellingtoncharlottenaaodarley@gmail.com>
2023-10-09 16:58:30 +0000

    Added new files

---
+++ README.md
@@ -0,0 +1,5 @@
+A decision making program with C.
+
+getchar together with switch statements were used for this program.
+
+Users are only allowed to type letters within the english alphabet, anything beyond that is flagged as an error.
---
+++ main.c
@@ -0,0 +1,115 @@
+#include <stdio.h>
+int main(){
+int c;
+printf("Type an alphabet:  ");
+c=getchar();
+switch(c){
+case 'A':
+case 'a':
+printf("You typed A");
+break;
+case 'B':
+case 'b':
+printf("You typed B");
+break;
+case 'C':
+case 'c':
+printf("You typed C");
+break;
+case 'D':
+case 'd':
+printf("You typed D");
+break;
+case 'E':
+case 'e':
+printf("You typed E");
+break;
+case 'F':
+case 'f':
+printf("You typed F");
+break;
+case 'G':
+case 'g':
+printf("You typed G");
+break;
+case 'H':
+case 'h':
+printf("You typed H");
+break;
+case 'I':
+case 'i':
+printf("You typed I");
+break;
+case 'J':
+case 'j':
+printf("You typed J");
+break;
+case 'K':
+case 'k':
+printf("You typed K");
+break;
+case 'L':
+case 'l':
+printf("You typed L");
+break;
+case 'M':
+case 'm':
+printf("You typed M");
+break;
+case 'N':
+case 'n':
+printf("You typed N");
+break;
+case 'O':
+case 'o':
+printf("You typed O");
+break;
+case 'P':
+case 'p':
+printf("You typed P");
+break;
+case 'Q':
+case 'q':
+printf("You typed Q");
+break;
+case 'R':
+case 'r':
+printf("You typed R");
+break;
+case 'S':
+case 's':
+printf("You typed S");
+break;
+case 'T':
+case 't':
+printf("You typed T");
+break;
+case 'U':
+case 'u':
+printf("You typed U");
+break;
+case 'V':
+case 'v':
+printf("You typed V");
+break;
+case 'W':
+case 'w':
+printf("You typed W");
+break;
+case 'X':
+case 'x':
+printf("You typed X");
+break;
+case 'Y':
+case 'y':
+printf("You typed Y");
+break;
+case 'Z':
+case 'z':
+printf("You typed Z");
+break;
+default:
+printf("Error!");
+}
+return(0);
+}
